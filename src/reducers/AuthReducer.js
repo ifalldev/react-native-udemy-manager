cb4868325c624cff43d87986f3dd7ccf931e1213
Caio Batista <cbatista.93@gmail.com>
2017-03-01 10:35:42 -0300

    add types.js; upgrate auth reducer; login form add map state to props

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,8 +1,12 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
 const INITIAL_STATE = { email: '' };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  default:
-    return state;
+    case EMAIL_CHANGED:
+      return { ...state, email: action.payload };
+    default:
+      return state;
   }
 };
