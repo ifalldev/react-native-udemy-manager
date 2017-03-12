8f97bbcadb60b5d42082475a4988196016fe6304
Caio Batista <cbatista.93@gmail.com>
2017-03-12 13:38:25 -0300

    begin login_user actions bug fix

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -15,7 +15,7 @@ class LoginForm extends Component {
   onButtonPress() {
     const { email, password } = this.props;
 
-    this.protps.loginUser({ email, password });
+    this.props.loginUser({ email, password });
   }
 
   render() {
