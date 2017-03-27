c569143053e278d1184e46894a5daaf13b435646
Caio Batista <cbatista.93@gmail.com>
2017-03-27 06:15:56 -0300

    auth actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,5 @@ export const PASSWORD_CHANGED = 'password_chagend';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAIL = 'login_user_fail';
 export const LOGIN_USER_STARTER = 'login_user_starter';
+
+export const EMPLOYEE_UPDATE = 'employee_update';
